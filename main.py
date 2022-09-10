8b70a87c324e3f9afb78e7a67182a6ae63c29dd7
Danz2021 <omerdan03@gmail.com>
2022-09-10 17:56:25 +0300

    add start and end input time clear input

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from pynput.keyboard import Key, Listener
+import logging
+from datetime import datetime
+
 import argparse
 
 import PySimpleGUI as sg
 
-
-
+ACTIVATION_BTN_TEXT = {False: "Start Input",
+                       True: "Finish Input"}
 
 def get_args():
     def str2bool(input_value):
@@ -20,32 +24,48 @@ def get_args():
 
     parser.add_argument('-debug', metavar='debug', default=False, type=str2bool,
                         help='option for running the GUI in debug mode. (default=False)')
+    parser.add_argument('-file', metavar='file_name', default="keylog.txt", type=str,
+                        help='The output file. (default=keylog.txt)')
     args = parser.parse_args()
     return args
 
 
 def main():
-
-    activation_btn_text = {
-        False: "Start Input",
-        True: "Finish Input"
-    }
-
     args = get_args()
 
+    logging.basicConfig(filename=args.file, level=logging.DEBUG,
+                        format=" %(asctime)s - %(message)s")
+
     input_active = False
+    start_input = None
+    end_input = None
     window = sg.Window(title='Password Authenticator',
                        layout=[[sg.Text('Input Password: '), sg.Input(None, key='input_field', disabled=not input_active)],
-                               [sg.Button(activation_btn_text[input_active], key='activation_button'),
-                                sg.Button('Submit password', key='submit_button'), sg.Button('Quit')]],
+                               [sg.Button(ACTIVATION_BTN_TEXT[input_active], key='activation_button'),
+                                sg.Button('Submit password', key='submit_button', disabled=input_active), sg.Button('Quit')]],
                        margins=(400, 300))
     while True:
         event, values = window.read(timeout=50)
 
         if event == "activation_button":
+            if input_active:
+                end_input = datetime.now()
+            else:
+                window['input_field'].update('')
+                start_input = datetime.now()
             input_active = not input_active
-            window['activation_button'].update(activation_btn_text[input_active])
+
+            window['activation_button'].update(ACTIVATION_BTN_TEXT[input_active])
             window['input_field'].update(disabled=not input_active)
+            window['submit_button'].update(disabled=input_active)
+
+        if event == "submit_button":
+            if not values['input_field']:
+                print("No input")
+            else:
+                print(f"start: {start_input.strftime('%H:%M:%S')}")
+                print(f"end: {end_input.strftime('%H:%M:%S')}")
+                print(values['input_field'])
 
         if event == sg.WINDOW_CLOSED or event == 'Quit':
             break
